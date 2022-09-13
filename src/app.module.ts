309c3fa26ffd1c0c81c496405007e7d5344c80df
MSenaaa <senamatheusjob@gmail.com>
2022-09-12 22:55:51 -0300

    conectando com o banco de dados

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { PlayersModule } from './players/players.module';
 
 @Module({
-  imports: [PlayersModule],
+  imports: [
+    PlayersModule,
+    MongooseModule.forRoot(
+      'mongodb+srv://msenaa:jFD9Tz3kH0wXZEpI@cluster0.jfvdtla.mongodb.net/?retryWrites=true&w=majority',
+      {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+      },
+    ),
+  ],
   controllers: [],
   providers: [],
 })
